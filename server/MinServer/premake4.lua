cfad5fba142726b8670796903a2ce5ad47151550
Vladimir Kiryushkin <kai@rhynn.net>
2011-09-14 16:31:03 +0400

    moved script from wiki

--- server/MinServer/premake4.lua
+++ server/MinServer/premake4.lua
@@ -1,89 +1,140 @@
-solution "FWServer"	
-	language "C++"	
-	configurations {"Release","Debug"}
-	linkoptions {"-lrt"}
-	
-	flags {"ExtraWarnings"}
+solution "FWServer" 
+    language "C++"  
+    configurations {"Release","Debug"}
+    linkoptions {"-lrt"}
 
-	configuration "Release"
-		flags {"OptimizeSpeed"}
 
-	configuration "Debug"
-		defines {"_DEBUG","DEBUG"}
-		flags {"Symbols"}		
+    flags {"ExtraWarnings"}
 
-	project "FWUtil"	
-		location "./FWUtil"
-		kind "SharedLib"
-		files {
-			"../FWUtil/**.h","../FWUtil/**.cpp"
-		}
-					
-	project "MinServer"	
-		location "./MinServer"
-		kind "SharedLib"
-		files {
-			"../MinServer/**.h","../MinServer/**.cpp"
-		}
+    configuration "Release"
+        flags {"OptimizeSpeed"}
 
-	project "FWWorld"
-		location "./FWWorld"	
-		kind "SharedLib"
-		includedirs {
-			"/usr/include/mysql",
-			"/usr/include/mysql++",
-			"../FWUtil"
-		}
-		libdirs {
-			"."
-		}
-		links {
-			"FWUtil",
-			"mysqlpp",			
-			"mysqlclient",			
-			"boost_regex"
-		}
-		files {
-			"../FWWorld/**.h","../FWWorld/**.cpp"
-		}	
+    configuration "Debug"
+        defines {"_DEBUG","DEBUG"}
+        flags {"Symbols"}       
 
-	project "FWServer"
-		kind "ConsoleApp"
-		includedirs { 
-			"../FWUtil",
-			"../MinServer",
-			"../FWWorld",
-			"/usr/include/mysql",
-			"/usr/include/mysql++"
-		}
-		libdirs {
-			".",
-		}
-		links {
-			"FWUtil",
-			"FWWorld",
-			"MinServer",			
-			"mysqlpp",			
-			"mysqlclient",			
-			"boost_regex"
-		}
-		files {
-			"../FWServer/**.h","../FWServer/**.cpp"
-		}
-	
-	project "FWShutdown"
-		--location "./FWShutdown"	
-		kind "ConsoleApp"
-		includedirs {
-			"../MinServer"
-		}
-		libdirs {
-			"."
-		}
-		links {
-			"MinServer",
-		}
-		files {
-			"../FWShutdown/**.h","../FWShutdown/**.cpp"
-		}	
 
+
+    project "MinServer" 
+        location "./MinServer"
+        kind "SharedLib"
+        targetname "rhynn-minserver"        
+        files {
+            "./MinServer/**.h","./MinServer/**.cpp"
+        }
+        postbuildcommands {"cp -f ../librhynn-minserver.so /usr/lib"}
+
+
+    project "FWUtil"    
+        location "./FWUtil"
+        kind "SharedLib"
+        targetname "rhynn-fwutil"       
+        includedirs {
+            "/usr/include/mysql",
+            "/usr/local/include/mysql++",
+        }
+        files {
+            "./FWUtil/**.h","./FWUtil/**.cpp"
+        }
+        postbuildcommands {"cp -f ../librhynn-fwutil.so /usr/lib"}
+
+    project "FWWorld"
+        location "./FWWorld"    
+        kind "SharedLib"
+        targetname "rhynn-fwworld"      
+        includedirs {
+            "./FWUtil",
+            "/usr/include/mysql",
+            "/usr/local/include/mysql++",
+        }
+        libdirs {
+            "/usr/local/lib"
+        }
+        links {
+            "FWUtil",
+            "mysqlpp",          
+            "mysqlclient",
+            "boost_regex"
+        }
+        files {
+            "./FWWorld/**.h","./FWWorld/**.cpp"
+        }   
+        postbuildcommands {"cp -f ../librhynn-fwworld.so /usr/lib"}
+
+
+    project "FWServer"
+        kind "SharedLib"
+        location "./FWWServer"
+        targetname "rhynn-fwserver"     
+        includedirs { 
+            "./MinServer",
+            "./FWUtil",
+            "./FWWorld",
+            "/usr/include/mysql",
+            "/usr/local/include/mysql++",
+            "/usr/local/include"
+        }
+        libdirs {
+            "/usr/local/lib"
+        }
+        links {
+            "MinServer",            
+            "FWUtil",
+            "FWWorld",
+            "mysqlclient",  
+            "mysqlpp",          
+            "boost_regex"
+        }
+        files {
+            "./FWServer/**.h","./FWServer/**.cpp"
+        }
+        postbuildcommands {"cp -f ../librhynn-fwserver.so /usr/lib"}
+
+    project "FWServerLauncher"  
+        kind "ConsoleApp"
+        location "./FWServerLauncher"
+        targetdir "."
+        targetname "ServerStart"
+        includedirs { 
+            "./FWUtil",
+            "./MinServer",
+            "./FWWorld",
+            "./FWServer",
+            "/usr/include/mysql",
+            "/usr/local/include/mysql++",
+            "/usr/local/include"
+        }
+        libdirs {
+            "/usr/local/lib"
+        }
+        links {
+            "MinServer",            
+            "FWUtil",
+            "FWWorld",
+            "FWServer",
+            "mysqlclient",          
+            "mysqlpp",          
+            "boost_regex"
+        }
+        files {
+            "./FWServerLauncher/**.h","./FWServerLauncher/**.cpp"
+        }
+
+
+    project "FWShutdown"
+        kind "ConsoleApp"
+        location "./FWShutdown" 
+        targetdir "."
+        targetname "ServerStop"
+        includedirs {
+            "./MinServer"
+        }
+        libdirs {
+            "/usr/local/lib"
+        }
+        links {
+            "MinServer",
+        }
+        files {
+            "./FWShutdown/**.h","./FWShutdown/**.cpp"
+        }
